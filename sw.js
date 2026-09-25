9843fcc9b8dc1566aea6b0416938721960561454
SararaX-hub <latestcaptaintz@gmail.com>
2026-09-25 12:16:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE='anima-v1';
+const ASSETS=['/SararaX/madeia-flow.html','/SararaX/manifest.json'];
+
+self.addEventListener('install',function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached||fetch(e.request).catch(function(){return caches.match('/SararaX/madeia-flow.html');});
+    })
+  );
+});
